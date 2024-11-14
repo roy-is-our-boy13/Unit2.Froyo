b3033b3491f9416b654b384dc1a075fe21b24f46
Roy <rjperlman@gmail.com>
2024-11-14 13:00:59 -0800

    Updating some methods.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+/**
+ * Some unnecessary lines of code are needed for Block 15 Workshop: Froyo Orders, but gaining hands-on 
+ * experience with them helped me get a good start on the workshop.
+ */
 const froyo = {
     flavor: ["Vanilla", "Strawberry", "Coffee", "Birthday Cake", "Chocolate"],
     size: ["Large", "Medium", "Small"]
@@ -6,12 +9,19 @@ const froyo = {
 
 console.log(Object.keys(froyo));
 console.log(Object.values(froyo));
+console.log(froyo);
 
 function displayTable(object)
 {
-    
+    for(const key in object)
+    {
+        console.log(key +": "+object[key]);
+    }
 }
 
+displayTable(froyo);
+
+//The primary lines of code for Block 15 Workshop: Froyo Orders.
 const userInputOnFroyoFlavor = prompt(
     "Please enter the froyo flavors you would like to order, separated by commas.",
     "Vanilla,vanilla,vanilla,strawberry,coffee,coffee,Birthday cAke,Birthday Cake"
@@ -22,6 +32,10 @@ const userInputOnFroyoFlavor = prompt(
   console.log(lowercaseInputOnFroyoFlavor);
 
   const stringArrayOfFroyoFlavors = lowercaseInputOnFroyoFlavor.split(",");
+
+  //const mainInputOnFroyoFlavor = stringArrayOfFroyoFlavors.map(flavor => flavor.charAt(0).toUpperCase() +  + flavor.slice(1));
+
+  //console.log(mainInputOnFroyoFlavor);
 
   function flavorAmount(flavors)
   {
